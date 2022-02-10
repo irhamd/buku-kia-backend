83c6d0d7305632dbb161e963cecae17dfcbf6179
irhamd <irham.pnm@gmail.com>
2022-02-10 08:25:45 +0800

    fff

--- app/Http/Controllers/PengaduanSimrs/DashboardController.php
+++ app/Http/Controllers/PengaduanSimrs/DashboardController.php
@@ -23,23 +23,27 @@ class DashboardController extends Controller
                 sum( case when progres = 'dn' then 1 else 0 end ) as selesai,
                 sum( case when progres <> 'dn' then 1 else 0 end ) as pending
             from(
-                SELECT * from pgd_pengaduan_t WHERE created_at BETWEEN '$req->tglawal' and '$tgl->tglakhir'
+                SELECT * from pgd_pengaduan_t WHERE created_at BETWEEN '$req->tglawal' and '$req->tglakhir'
             and aktif = '1'
             ) as rr
         ");
 
         $datachart = DB::select("
+          
+
+
             SELECT 
-                tgl,
+                tgl, bulan,
                 count( id) as jumlah,
                 0 as byapp,
                 count( id) as bycall,
                 sum( case when progres = 'dn' then 1 else 0 end ) as selesai,
                 sum( case when progres <> 'dn' then 1 else 0 end ) as pending
             from(
-                SELECT * , extract( day from  created_at ) AS  tgl from pgd_pengaduan_t WHERE created_at BETWEEN '$req->tglawal' and '$tgl->tglakhir' and aktif = '1'
+                SELECT extract( day from  created_at ) AS  tgl, extract( MONTH from created_at ) AS  bulan,  *  from pgd_pengaduan_t WHERE created_at  BETWEEN '$req->tglawal' and '$req->tglakhir' and aktif = '1' 
             ) as rr
-            GROUP BY tgl ORDER BY tgl
+            GROUP BY bulan, tgl order BY bulan, tgl
+
         ");
 
 
@@ -47,9 +51,8 @@ class DashboardController extends Controller
             SELECT  unitkerja , count( unitkerja) as jumlah
             from(
                 SELECT  pdt.*  from pgd_pengaduan_t as pdt 
-                WHERE pdt.created_at BETWEEN '$req->tglawal' and '$tgl->tglakhir' and pdt.aktif = '1'
+                WHERE pdt.created_at BETWEEN '$req->tglawal' and '$req->tglakhir' and pdt.aktif = '1'
             ) as rr GROUP BY unitkerja ORDER BY jumlah desc limit 8
-            
         ");
         $databypetugas = DB::select("
                 SELECT 
@@ -57,7 +60,7 @@ class DashboardController extends Controller
             from(
                 SELECT pg.namapegawai ,  pdt.*  from pgd_pengaduan_t as pdt 
                 left join pegawai_m as pg on pg.id = pdt.assignto
-                WHERE pdt.created_at BETWEEN '$req->tglawal' and '$tgl->tglakhir' and pdt.aktif = '1'
+                WHERE pdt.created_at BETWEEN '$req->tglawal' and '$req->tglakhir' and pdt.aktif = '1'
             ) as rr
             GROUP BY namapegawai
             ORDER BY jumlah desc 
